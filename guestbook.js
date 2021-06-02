ce5c4561fb1a8a4eb10aec4a5549fc346f9ad310
melbelen <78703663+melbelen@users.noreply.github.com>
2021-06-02 07:25:29 -0400

    Update guestbook.js
    update to apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,8 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://c7548fd3.us-south.apigw.appdomain.cloud/guestbook
+';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
